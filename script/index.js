a70c7f9a4e8076989da62ecc89231f6e97122526
imranxony <amran584044@gmail.com>
2025-09-10 18:42:40 +0100

    33-5 Design & Display Dynamic cards and Recap Everything

--- script/index.js
+++ script/index.js
@@ -12,12 +12,31 @@ const loadlevelword = (id) => {
 
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
-  // wordContainer.innerHTML = "";
+  wordContainer.innerHTML = "";
+  // "id": 4,
+  // "level": 5,
+  // "word": "Diligent",
+  // "meaning": "পরিশ্রমী",
+  // "pronunciation": "ডিলিজেন্ট"
   words.forEach((word) => {
     console.log(word);
     const card = document.createElement("div");
     card.innerHTML = `
-    <p> cat </p>
+    <div
+        class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4"
+      >
+        <h2 class="font-bold text-2xl">${word.word}</h2>
+        <p class="font-semibold">meaning/ pronounction</p>
+        <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-circle-info"></i>
+          </button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+        </div>
+      </div>
     `;
     wordContainer.append(card);
   });
